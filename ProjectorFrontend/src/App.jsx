9d3f94dd87a4fef9046840727c8e516273fadfb8
Khnzh <n.g.khunjua@gmail.com>
2024-07-02 04:06:57 +0400

    profile part 80% done

--- ProjectorFrontend/src/App.jsx
+++ ProjectorFrontend/src/App.jsx
@@ -1,39 +1,20 @@
-import supabase from './config/supabaseClient';
-import {BrowserRouter, Router, Link, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Landing from './Landing';
 import Login from './Login';
 import Signup from './Signup';
-import { useAppContext } from './context/AuthContext';
+import Profile from './Profile';
+import Header from './Header';
 
 export default function App() {
-  const { isLoggedIn, setIsLoggedIn } = useAppContext();
-
-  const logoutUser = async (e) =>  {
-    e.preventDefault();
-    let { error } = await supabase.auth.signOut();
-    setIsLoggedIn(false);
-  }
-
-return (
+  return (
     <BrowserRouter>
-      <div className="header">
-        <ul className="nav-bar">
-          <li className="nav-link"><Link to='/'>О НАС</Link></li>
-          <li className="nav-link"><Link to='/'>ПОЛЬЗОВАТЕЛЯМ</Link></li>
-          <li className="nav-link"><Link to='/'>ПАРТНЕРАМ</Link></li>
-          <li className="nav-link"><Link to='/'>КОНТАКТЫ</Link></li>
-          {isLoggedIn?(
-            <li className="sign"><a onClick={logoutUser} href="#">ВЙТИ</a></li>
-          ):(
-            <li className="sign"><Link to='/login'>ВОЙТИ В АККАУНТ</Link></li>
-          )}
-        </ul>
-      </div>
+      <Header />
       <Routes>
-        <Route path='/' element={<Landing/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/' element={<Landing />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </BrowserRouter>
-)
+  );
 }
