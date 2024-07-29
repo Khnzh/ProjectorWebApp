e31b39ef84852ae4f98f502535a4ecd6a159995d
Khnzh <n.g.khunjua@gmail.com>
2024-07-29 20:35:40 +0300

    fixed small bug

--- ProjectorFrontend/src/utilityFunctions/Validation.js
+++ ProjectorFrontend/src/utilityFunctions/Validation.js
@@ -45,19 +45,19 @@ export function profileValidation(value){
 
     if (hasOnlySpecificStrings(value, '')) return errors;
 
-    if (value.name && value.name.length > 0){
+    if (value.name){
         if (!nameRegex.test(value.name)) errors.name = 'Имя должно содержать не больше 10 букв'
     } else {
         errors.name = 'Заполните поле'
     }
 
-    if (value.lastName && value.lastName.length > 0){
+    if (value.lastName){
         if (!lastNameRegex.test(value.lastName)) errors.lastName = 'Фамилия должна содержать не больше 20 букв'
     } else {
         errors.lastName = 'Заполните поле'
     }
 
-    if (value.bio && value.bio.length > 0){
+    if (value.bio){
         if (whitelistRegex.test(value.bio) && !blacklistRegex.test(value.bio)) {
             if (value.bio.length > 250) {errors.bio = 'Используйте не больше 250 символов'}
         } else errors.bio = `Используйте только буквы и специальные символы`;
@@ -65,25 +65,25 @@ export function profileValidation(value){
     
     if (value.langs && value.langs.length > 5) errors.langs = 'Выберите не больше пяти';
 
-    if (value.phNumber && value.phNumber.length > 0){
+    if (value.phNumber){
         if (!phonePatterns.some(pattern => pattern.test(value.phNumber))) {
             errors.phNumber = `Некорректный номер`
         }
     }
 
-    if (value.socials && value.socials.length > 0){
+    if (value.socials){
         if (!socialsPatterns.some(pattern => pattern.test(value.socials))) {
             errors.socials = `Некорректная ссылка профиля`
         }
     }
 
-    if (value.specialties && value.specialties.length > 0){
+    if (value.specialties){
         if (value.specialties.length > 5) errors.specialties = 'Выберите не больше пяти'
     } else {
         errors.specialties = 'Заполните поле'
     }
 
-    if (value.tg && value.tg.length>0){
+    if (value.tg){
         if (!telegramPattern.test(value.tg)){
             errors.tg = `Некорректная ссылка профиля`
         }
@@ -95,25 +95,25 @@ export function profileValidation(value){
 export function signupValidation(value){
     const errors = {};
 
-    if (value.name && value.name.length > 0){
+    if (value.name){
         if (!nameRegex.test(value.name)) errors.name = 'Имя должно содержать не больше 10 букв'
     } else {
         errors.name = 'Заполните поле'
     }
 
-    if (value.lastName && value.lastName.length > 0){
+    if (value.lastName){
         if (!lastNameRegex.test(value.lastName)) errors.lastName = 'Фамилия должна содержать не больше 20 букв'
     } else {
         errors.lastName = 'Заполните поле'
     }
 
-    if (value.mail && value.mail.length > 0){
+    if (value.mail){
         if (!emailPattern.test(value.mail) || blacklistRegex.test(value.mail)) errors.mail = 'Введите корректную почту'
     } else {
         errors.mail = 'Заполните поле'
     }
 
-    if (value.dob && value.dob.length > 0){
+    if (value.dob){
         const difference = new Date() - new Date(value.dob)
         const hundredYears = 3155695200000
         const fourteenYears = 441797328000
@@ -122,7 +122,7 @@ export function signupValidation(value){
         errors.dob = 'Заполните поле'
     }
 
-    if (value.password && value.password.length > 0){
+    if (value.password){
         if (!passwordPattern.test(value.password) || blacklistRegex.test(value.password)) {
             errors.password = 'Слабый пароль';
         }
@@ -152,7 +152,7 @@ export function portfolioValidation(value, i){
     if (hasOnlySpecificStrings(value, '')) return errors;
 
     for (let iteration = 0; iteration <= i; iteration++) {
-        if (value.name[iteration].length > 0){
+        if (value.name[iteration]){
             if (!nameRegex.test(value.name[iteration])) {errors.prName = [...errors.prName, 'Имя должно содержать не больше 10 букв']}
             else {errors.prName = [...errors.prName, '']}
         } else {
@@ -166,7 +166,7 @@ export function portfolioValidation(value, i){
             } else errors.desc = [...errors.desc, `Используйте только буквы и специальные символы`];
         } else {errors.desc = [...errors.desc, '']}
 
-        if (value.role[iteration].length > 0){
+        if (value.role[iteration]){
             if (whitelistRegex.test(value.role[iteration]) && !blacklistRegex.test(value.role[iteration])) {
                 if (value.role[iteration].length > 30) {errors.role = [...errors.role, 'Используйте не больше 300 символов']}
                 else {errors.role = [...errors.role, '']}
@@ -184,7 +184,7 @@ export function portfolioValidation(value, i){
             errors.link = [...errors.link, '']
         }
 
-        if (value.year[iteration].length >0){
+        if (value.year[iteration]){
             if (value.year[iteration]<1950 || value.year[iteration]> new Date().getFullYear()) {errors.year = [...errors.year, 'Введите корректную дату в формате ГГГГ']}
             else {errors.year = [...errors.year, '']}
         } else {
@@ -214,7 +214,7 @@ export function educationValidation(value, i){
             errors.eduType = [...errors.eduType, 'Заполните поле']
         }
 
-        if (value.mastery[iteration].length > 0){
+        if (value.mastery[iteration]){
             if (whitelistRegex.test(value.mastery[iteration]) && !blacklistRegex.test(value.mastery[iteration])) {
                 if (value.mastery[iteration].length > 50) {errors.mastery = [...errors.mastery, 'Используйте не больше 50 символов']}
                 else {errors.mastery = [...errors.mastery, '']}
@@ -223,7 +223,7 @@ export function educationValidation(value, i){
             errors.mastery = [...errors.mastery, '']
         }
 
-        if (value.faculty[iteration].length > 0){
+        if (value.faculty[iteration]){
             if (whitelistRegex.test(value.faculty[iteration]) && !blacklistRegex.test(value.faculty[iteration])) {
                 if (value.faculty[iteration].length > 50) {errors.faculty = [...errors.faculty, 'Используйте не больше 50 символов']}
                 else {errors.faculty = [...errors.faculty, '']}
@@ -232,7 +232,7 @@ export function educationValidation(value, i){
             errors.faculty = [...errors.faculty, 'Заполните поле']
         }
 
-        if (value.facility[iteration].length > 0){
+        if (value.facility[iteration]){
             if (whitelistRegex.test(value.facility[iteration]) && !blacklistRegex.test(value.facility[iteration])) {
                 if (value.facility[iteration].length > 50) {errors.facility = [...errors.facility, 'Используйте не больше 50 символов']}
                 else {errors.facility = [...errors.facility, '']}
@@ -241,14 +241,14 @@ export function educationValidation(value, i){
             errors.facility = [...errors.facility, 'Заполните поле']
         }
 
-        if (value.grad[iteration].length >0){
+        if (value.grad[iteration]){
             if (value.grad[iteration]<1950 || value.grad[iteration]> new Date().getFullYear()) {errors.grad = [...errors.grad, 'Введите корректную дату в формате ГГГГ']}
             else {errors.grad = [...errors.grad, '']}
         } else {
             errors.grad = [...errors.grad,'Заполните поле']
         }
 
-        if (value.enrollment[iteration].length >0){
+        if (value.enrollment[iteration]){
             if (value.enrollment[iteration]<1950 || value.enrollment[iteration]> new Date().getFullYear()) {errors.enrollment = [...errors.enrollment, 'Введите корректную дату в формате ГГГГ']}
             else {errors.enrollment = [...errors.enrollment, '']}
         } else {
