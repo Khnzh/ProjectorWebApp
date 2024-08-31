6466ec29d944ea57f56a3e7f0ed915ce119d7366
Khnzh <n.g.khunjua@gmail.com>
2024-08-31 21:13:03 +0400

    sidebar + displayProjects in account done

--- ProjectorFrontend/src/pages/projectDisplay/ProjectDisplay.jsx
+++ ProjectorFrontend/src/pages/projectDisplay/ProjectDisplay.jsx
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
+import supabase from '../../config/supabaseClient';
 
 import styles from './ProjectDisplay.module.scss';
+import Popover_fiter from '../../components/popover/Popover';
 
 function ProjectDisplay(props) {
+  const [projectInfo, setProjectInfo] = useState();
+
+  useEffect(()=>
+    { (async () => {const { data, error } = await supabase
+  .from('Projects')
+  .select(`
+    id,
+    name,
+    description,
+    created_at,
+    Profile ( id, name, lastName )
+  `);
+
+  
+  if (error) {
+    console.error('Error fetching data:', error);
+  } else {
+    console.log('Fetched projects with qualifications:', data);
+    setProjectInfo(data);
+  }})()
+  
+}, [])
+
+function defaultImage (e) {
+  e.target.src='/assets/hlopushka.svg';
+}
+
   return (
 
             <>
-              <div className={styles.project_card} >
+              <div className={styles.search_and_add}>
+                <div className={styles.search_container}>
+                  <input type="text" className={cn('login__input', styles.searchbar)} placeholder='Поиск'/>
+                  <button className={styles.inv_search_button}> </button>
+                </div>
+                <button className={styles.button_accent}>holy moly</button>
+              </div>
+              <Popover_fiter/>
+              {projectInfo && projectInfo.map((item) =>
+                <>
+                <div className={styles.project_card} >
                 <div className={styles.img_frame}>
-                <img src="/assets/hlopushka.svg" alt="hlopushka" />
+                  <img src={`https://rotyixpntplxytekbeuz.supabase.co/storage/v1/object/public/project_photos/${item.Profile.id}/${item.id}/Project_pic.png`} alt="hlopushka" onError={(e)=>(defaultImage(e))}/>
                 </div>
                 <div className={styles.project_text} >
-                  <h1 className={styles.title4}>ЦЕХОВЫЙ МАЛЬЧИШКА</h1>
-                  <h2 className={styles.medium_title3}>Володя Александров</h2>
-                  <h3 className={styles.subtitle}>
-                    Геня Жульшенков попадает по распределению на работу в цех, в котором царит безвластие и
-                    неорганизованность
-                  </h3>
+                  <h1 className={styles.title4}>{item.name}</h1>
+                  <h2 className={styles.medium_title3}>{item.Profile.name} {item.Profile.lastName}</h2>
+                  <div className={styles.desc_cont}>
+                    <h3 className={styles.subtitle}>
+                      {item.description}
+                    </h3>
                   </div>
+                </div>
               </div>
+                </>
+              )}
             </>
   );
 }
