3cd467acf7717dff461e4fee4759c5440595535b
Khnzh <n.g.khunjua@gmail.com>
2024-09-30 20:13:21 +0400

    doing some job on refactoring

--- ProjectorFrontend/src/pages/landing/Landing.jsx
+++ ProjectorFrontend/src/pages/landing/Landing.jsx
@@ -1,22 +1,45 @@
-import { useState } from 'react'
-import styles from './Landing.module.scss'
-import.meta.env.VITE_SUPABASE_KEY
+import { useState } from "react";
+import styles from "./Landing.module.scss";
 
 function Landing() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div className={styles.container}>
         <img src="/projector.svg" alt="" className="big-header" />
         <div className={styles.landing_cmps}>
-        <img src="/cloudFrame.svg" alt="" className="cloud" />
-        <img src="/circlesComponent.svg" alt="" className="circles" />
-        <p className={styles.cloud_text}>Камера, мотор… ПРОЖЕКТОР!<br/>Найди подходящего специалиста сейчас!</p>
+          <img src="/cloudFrame.svg" alt="" className="cloud" />
+          <img src="/circlesComponent.svg" alt="" className="circles" />
+          <p className={styles.cloud_text}>
+            Камера, мотор… ПРОЖЕКТОР!
+            <br />
+            Найди подходящего специалиста сейчас!
+          </p>
+        </div>
+        <img src="/projector.svg" alt="" className="big-header" />
+        <div className={styles.landing_cmps}>
+          <img src="/cloudFrame.svg" alt="" className="cloud" />
+          <img src="/circlesComponent.svg" alt="" className="circles" />
+          <p className={styles.cloud_text}>
+            Камера, мотор… ПРОЖЕКТОР!
+            <br />
+            Найди подходящего специалиста сейчас!
+          </p>
+        </div>
+        <img src="/projector.svg" alt="" className="big-header" />
+        <div className={styles.landing_cmps}>
+          <img src="/cloudFrame.svg" alt="" className="cloud" />
+          <img src="/circlesComponent.svg" alt="" className="circles" />
+          <p className={styles.cloud_text}>
+            Камера, мотор… ПРОЖЕКТОР!
+            <br />
+            Найди подходящего специалиста сейчас!
+          </p>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
